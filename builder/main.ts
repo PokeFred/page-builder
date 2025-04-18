4a7fe888969419ac326ba47bc9c65aadf9624f62
PokeFred <Cedric.paelmke@gmx.de>
2025-04-19 00:34:48 +0200

    update

--- builder/main.ts
+++ builder/main.ts
@@ -8,7 +8,19 @@ async function main(): Promise<void> {
     }
 
     mkdirSync(`${path}/build`, { recursive: true })
-    writeFileSync(`${path}/build/index.html`, "<div>Test</div>", { encoding: "utf-8" })
+    writeFileSync(`${path}/build/index.html`, `
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Document</title>
+            </head>
+            <body>
+                <div>Hello World</div>
+            </body>
+        </html>
+    `, { encoding: "utf-8" })
 }
 
 main()
